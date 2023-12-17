ce503b41d57b84ef8298eccdcb114542158d2825
rahimiairfhussain <arifhussain.rahimi84@gmail.com>
2023-12-17 22:02:39 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         this is the first test
+        </p>
+       <h1>
+         hi this is the first test of the react native
+       </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
